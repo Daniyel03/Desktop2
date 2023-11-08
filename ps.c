1b114a4f68b61a82f9147442dbb4bcf7e21dd970
Daniyel03 <daniel.scholz05@gmail.com>
2023-11-08 14:44:57 +0100

    lol

---
+++ ps.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int	subcounter(const char *s, char c)
+{
+	int	subs;
+
+	subs = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			subs++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (subs);
+}
+
+void	free_split(char **res)
+{
+	int	i;
+
+	if (!res)
+		return ;
+	i = 0;
+	while (res[i] != NULL)
+	{
+		free(res[i]);
+		i++;
+	}
+	free(res);
+}
+
+char	*extract_substring(const char *s, char c)
+{
+	const char	*start;
+	int			len;
+	char		*substring;
+	int			i;
+
+	start = s;
+	while (*s && *s != c)
+		s++;
+	len = s - start;
+	substring = (char *)malloc((len + 1) * sizeof(char));
+	if (!substring)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substring[i] = start[i];
+		i++;
+	}
+	substring[len] = '\0';
+	return (substring);
+}
+
+static int	ft_split2(char const *s, char c, char **res)
+{
+	int	i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+		{
+			res[i] = extract_substring(s, c);
+			if (!res[i])
+			{
+				free_split(res);
+				return (0);
+			}
+			i++;
+			while (*s && *s != c)
+				s++;
+		}
+	}
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		subs;
+	char	**res;
+
+	subs = subcounter(s, c);
+	res = (char **)malloc((subs + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	if (ft_split2(s, c, res))
+		res[subs] = (NULL);
+	else
+		return (NULL);
+	return (res);
+}
+
+    // write a struct to perfome actions with my stack
+    typedef struct s_stack
+{
+    int     *stack;
+    int     top;
+    int     size;
+}   t_stack;
+
+
+int main(int argc, char **argv)
+{
+    if (1 == argc || (2 == argc && ! argv[1][0]))
+        return 1;
+    else if (2 == argc)
+        argv = ft_split(argv[1], ' ');
+    // write a loop to printf my **argv
+            int i = 0;
+    while (argv[i])
+    {
+        printf("%s\n", argv[i]);
+        i++;
+    }
+    return (0);
+}
