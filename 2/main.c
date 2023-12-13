56720b35d033260039ce89d8c3709a559defb5e4
Daniyel03 <daniel.scholz05@gmail.com>
2023-12-13 11:18:05 +0100

    n

--- 2/main.c
+++ 2/main.c
@@ -6,10 +6,11 @@
 /*   By: dscholz <dscholz@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 17:35:13 by dscholz           #+#    #+#             */
-/*   Updated: 2023/12/12 10:32:02 by dscholz          ###   ########.fr       */
+/*   Updated: 2023/12/13 11:16:36 by dscholz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf/ft_printf.h"
 #include "push_swap.h"
 
 int	ft_atoi(const char *str)
@@ -87,27 +88,90 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
 
+int ft_isdigit(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else
+			return 0;
+	}
+	return 1;
+}
+int max2(int count, char** arg)
+{
+	int i = count;
+	while (count-- > 0) // pb with count when split or not 
+    {
+        char* convertedString = ft_itoa(ft_atoi(arg[count]));
+
+        if (ft_strncmp(arg[count], convertedString, 11) != 0)
+        {
+            free(convertedString); // Free allocated memory
+				while(i--)
+					free(arg[i]);
+				free(arg);
+            	return 0;
+		}
+        else
+        {
+            free(convertedString); // Free allocated memory
+        }
+    }
+	while(i--)
+		free(arg[i]);
+	free(arg);
+	return 1;
+}
+
 int max(int count, char** arg)
 {
-    while (count > 1)
+	int i = 0;
+	int flag = 0;
+	if (count == 2 && !ft_isdigit(arg[1]))
+	{
+		arg = ft_split(arg[1], ' ');
+		while (arg[i])
+			i++;
+		if (!max2(i, arg))
+			return 0;
+		else
+			return 1;
+	}
+	count--;
+		ft_printf("count: %d\n", count);
+	// ft_printf("i: %d\n", i);
+	while (count-- > 1) // pb with count when split or not  0 1
     {
-        count--;
-
         char* convertedString = ft_itoa(ft_atoi(arg[count]));
 
         if (ft_strncmp(arg[count], convertedString, 11) != 0)
         {
             // free(arg[count]);
             free(convertedString); // Free allocated memory
-            return 0;
-        }
+			if (flag)
+			{
+				while(i--)
+					free(arg[i]);
+				free(arg);
+			}            // free(arg[count]);
+        	return 0;
+		}
         else
         {
             // free(arg[count]);
             free(convertedString); // Free allocated memory
         }
     }
-    return 1;
+	if (flag)
+	{
+		while(i--)
+			free(arg[i]);
+		free(arg);
+	}
+	return 1;
 }
 
 
@@ -116,16 +180,24 @@ static int justnum(char **str)
 	int i;
 	int j;
 
-	i = 0;
+	i = 1;
 	while(str[i])
 	{
 		j = 0;
+		while (str[i][j] == ' ')
+			j++;
+		if (str[i][j] == '\0')
+			return (0);
+		j = 0;
 		while(str[i][j])
 		{
-			if ((str[i][j] >= 97 && str[i][j] <= 122) || (str[i][j] >= 65 && str[i][j] <= 90) || (str[i][j] >= '0' && str[i][j] <= '9'))
-				return 0;
-			j++;
+			// ft_printf("str[i][j]: %c\n", str[i][j]);
+			if ((str[i][j] >= '0' && str[i][j] <= '9') || (str[i][j] == '-' || str[i][j] == '+' || str[i][j] == ' '))
+				j++;
+			else
+				return (0);
 		}
+		// ft_printf("i: %d\n", i);
 		i++;
 	}
 	return 1;
@@ -137,13 +209,15 @@ int	main(int argc, char **argv)
 
 	if (1 == argc || (2 == argc && !argv[1][0]))
 		return (1);
+	if (!justnum(argv))
+		return (ft_printf("schweizer1"), 0);
 	if (!max(argc, argv))
-		return (ft_printf("schweizer"), 0);
+		return (ft_printf("schweizer2"), 0);
 	else if (2 == argc)
 		argv = ft_split(argv[1], ' ');
 	else
 		argv = notsplit(argv, argc);
-	if (!argv || justnum(argv))
+	if (!argv)
 		return (0);
 	stack = stopfer(argv);
 	if (!asorted(stack))
